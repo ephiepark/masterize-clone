bb3ddba93b627ecd40f6cf7a497abe93cf63bd1c
ephiepark <ephiepark@ephieparks-MacBook-Air.local>
2019-03-02 01:07:54 -0800

    Crud progress page

--- app/utils/HistoryUtils.js
+++ app/utils/HistoryUtils.js
@@ -10,3 +10,27 @@ export function getConsequtiveSuccessCount(history) {
   }
   return count;
 }
+
+export function getNoteResultCountMap(history) {
+  const noteResultCountMap = new Map();
+  for (const record of history) {
+    if (!noteResultCountMap.has(record.noteQuestioned)) {
+      noteResultCountMap.set(record.noteQuestioned, [0, 0]);
+    }
+    const noteResultCount = noteResultCountMap.get(record.noteQuestioned);
+    const countCorrect = noteResultCount[0];
+    const countWrong = noteResultCount[1];
+    if (record.noteQuestioned === record.noteUserAnswer) {
+      noteResultCountMap.set(record.noteQuestioned, [
+        countCorrect + 1,
+        countWrong
+      ]);
+    } else {
+      noteResultCountMap.set(record.noteQuestioned, [
+        countCorrect,
+        countWrong + 1
+      ]);
+    }
+  }
+  return noteResultCountMap;
+}
